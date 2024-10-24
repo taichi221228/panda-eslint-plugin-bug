057fe5e0b709bce947617524298acdf15b4441fc
たいち ひ <taichi221228@icloud.com>
2024-10-24 15:19:28 +0900

    chore(linter): Comment out unused plugins and simplify ESLint configs
    Deactivate the panda plugin setup to avoid import errors and remove unused react plugins from the react ESLint configuration.

--- eslint.config.panda.js
+++ eslint.config.panda.js
@@ -1,8 +1,10 @@
+// Note: The error occurs just by importing without setting up the plugin.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import pandaPlugin from "@pandacss/eslint-plugin";
 
 export const pandaConfigs = [
-	{
-		plugins: { "@pandacss": pandaPlugin },
-		rules: pandaPlugin.configs.recommended.rules,
-	},
+	// {
+	// 	plugins: { "@pandacss": pandaPlugin },
+	// 	rules: pandaPlugin.configs.recommended.rules,
+	// },
 ];
--- eslint.config.react.js
+++ eslint.config.react.js
@@ -1,20 +1,9 @@
 import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import reactRefreshPlugin from "eslint-plugin-react-refresh";
 
 export const reactConfigs = [
 	reactPlugin.configs.flat.recommended,
 	reactPlugin.configs.flat["jsx-runtime"],
-	{
-		plugins: { "react-hooks": reactHooksPlugin },
-		rules: reactHooksPlugin.configs.recommended.rules,
-	},
-	{
-		plugins: { "react-refresh": reactRefreshPlugin },
-		rules: { "react-refresh/only-export-components": "warn" },
-	},
-	jsxA11yPlugin.flatConfigs.recommended,
 	{
 		files: ["**/*.{js,jsx,ts,tsx}"],
 		settings: {
@@ -22,5 +11,5 @@ export const reactConfigs = [
 			"import/resolver": { typescript: {} },
 		},
 	},
-
+	jsxA11yPlugin.flatConfigs.recommended,
 ];
